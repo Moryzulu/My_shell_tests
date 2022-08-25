c4dbbf80ae28fcb77b49d6287201d7e2ba1bb50c
moryzulu <morymacho6@gmail.com>
2022-08-25 12:02:57 -0700

    Test files for shell project

---
+++ Exercise_tasks/README.md
@@ -0,0 +1,3 @@
+Readme file for sample shell repository.
+this repositore will be my road map to the shell tasks and other necessary information.
+I will create files to sample each system call and library functions needed for this project.
---
+++ Exercise_tasks/get_ppid-test.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * Main - function to print laptop details that are in stock
+ *
+ * Return: void
+ */
+int main(void)
+{
+        struct laptop {
+
+                char model[20];
+                float cpu_speed;
+                int hdd_size;
+                float cost;
+
+        };
+
+        pid_t ppid;
+        ppid = getppid();
+
+
+        struct laptop detail1 = {"ASUS", 1.6, 250, 1800};
+        struct laptop detail2 = {"Toshiba Satelite", 2.0, 500, 4500};
+        struct laptop detail3 = {"HP Probook", 1.6, 250, 4000};
+
+        printf("\nHere is what your laptop details are:\n\n");
+        printf("Model: %s\n Cpu: %0.2fghz\n Hdd: %dGB\n Cost: K%6.2f\n\n", detail1.model, detail1.cpu_speed, detail1.hdd_size, detail1.cost);
+        printf("Model: %s\n Cpu: %0.2fghz\n Hdd: %dGB\n Cost: K%6.2f\n\n", detail2.model, detail2.cpu_speed, detail2.hdd_size, detail2.cost);
+        printf("Model: %s\n Cpu: %0.2fghz\n Hdd: %dGB\n Cost: K%6.2f\n\n", detail3.model, detail3.cpu_speed, detail3.hdd_size, detail3.cost);
+        printf("ppid is %u\n\n", ppid);
+
+        return (0);
+}
